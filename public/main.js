b1523c6c7586143010576377580d880a9c130c53
yadavanil626 <yadavanil626@github.com>
2020-05-27 19:20:34 +0530

    up

--- public/main.js
+++ public/main.js
@@ -12,7 +12,7 @@ console.log(navigator.mediaDevices.getSupportedContraints.supports['facingMode']
 
   
 //get stream
-navigator.mediaDevices.getUserMedia({video:false, audio:true})
+navigator.mediaDevices.getUserMedia({video:true, audio:true})
     .then(stream => {
         socket.emit('NewClient')
         video.srcObject = stream

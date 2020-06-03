a6125f02e4a4377262c7c92884ae1ed5f4095f34
yadavanil626 <yadavanil626@github.com>
2020-06-03 14:23:43 +0530

    Up and Running

--- public/bundle.js
+++ public/bundle.js
@@ -8115,7 +8115,7 @@ let currentFilter
 console.log(navigator.mediaDevices.getSupportedContraints.supports['facingMode']) */
 
 const videoConstraints = {
-    facingMode:'environment'
+    facingMode:'user'
   };
 const constraints = {
     video: videoConstraints,
